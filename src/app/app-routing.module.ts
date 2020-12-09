e2c00e6b7460a3e656aa8a60cb5082be6223bf00
Luca Ren <renluca93@gmail.com>
2020-12-09 15:48:55 +0100

    added pipes for type of pokemon and route for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DetailPokemonComponent} from './pokemons/detail-pokemon/detail-pokemon.component';
+import {listPokemonComponent} from './pokemons/list-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pokemon', component: listPokemonComponent },
+  {path: 'pokemon/:id', component: DetailPokemonComponent },
+  {path: '', redirectTo: 'pokemon', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pokemons/list-pokemon.component.ts
+++ src/app/pokemons/list-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../shared/list.pokemon';
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -15,8 +16,12 @@ export class listPokemonComponent implements OnInit {
     this.pokemons = POKEMONS;
   }
 
+  constructor(private router: Router) {
+  }
   selectPokemon(pokemon: Pokemon): void {
     alert('Vous avez séléctionné: ' + pokemon.name);
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
   }
 }
 
